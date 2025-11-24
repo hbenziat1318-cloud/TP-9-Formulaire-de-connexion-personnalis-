dcd27e473f4e431f34ea86235cad19e68e77fe0f
hbenziat1318-cloud <h.benziat1318@uca.ac.ma>
2025-11-24 23:33:51 +0100

    Add files via upload

---
+++ TP9 security-login-master/src/test/java/ma/fstg/security_login/SecurityLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.fstg.security_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
